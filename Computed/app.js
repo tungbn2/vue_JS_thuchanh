608166ba91780f0875de69034e443daa2bdeeeb2
tungbn <79492068+tungbn2@users.noreply.github.com>
2021-06-12 18:15:11 +0700

    Computed, dữ liệu 2 chiều

--- Computed/app.js
+++ Computed/app.js
@@ -1,25 +1,41 @@
-let vm = new Vue ({
-    el: `#app`,
-    data: {
-        msg: `Hello World`,
-        a: 0,
-        b: 0,
-        number: 20,
-    },
-    methods: {
-        addA () {
-            console.log("add A");
-            return this.a+this.number;
-        },
-        addB  () {
-            console.log("add B");
-            return this.b+this.number;
-        },
-    },
-    computed: {
-        reversedMessage: function () {
-            // `this` points to the vm instance
-            return this.msg.split('').reverse().join('');
-          }
+let vm = new Vue({
+  el: `#app`,
+  data: {
+    msg: `Hello World`,
+    a: 0,
+    b: 0,
+    number: 20,
+
+    firstName: ``,
+    name: ``,
+
+  },
+  methods: {
+    // addA () {
+    //     console.log(this.a);
+    //     return this.a+this.number;
+    // },
+    // addB  () {
+    //     console.log("add B");
+    //     return this.b+this.number;
+    // },
+
+    handleKeyUp (e) {
+        return this.firstName = e.target.value;
     }
+  },
+  computed: {
+    reversedMessage: function () {
+        // `this` points to the vm instance
+        return this.msg.split('').reverse().join('');
+    },
+    addA () {
+      console.log(this.a);
+      return this.a + this.number;
+    },
+    addB: function () {
+      console.log(this.b);
+      return this.b + this.number;
+    },
+  },
 });
